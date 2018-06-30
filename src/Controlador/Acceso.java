28eaa1d86aadc26efeb0426feea8ada5f17544e2
krlos <krlos@krlos-PC>
2018-06-30 19:29:26 -0430

    la clase en donde esta el main es Acceso

---
+++ src/Controlador/Acceso.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+import Controlador.*;
+/**
+ *
+ * @author krlos
+ */
+public class Acceso {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ctrl_Reportes.getInstance().i_Menu();                    //INicia ejecucion de aplicacion
+    }
+    
+}
