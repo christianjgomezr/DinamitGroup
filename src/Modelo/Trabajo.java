f999208f77f7e929ea7d2c7e4107f68fb5ee873f
Krkas <40615888+Krkas@users.noreply.github.com>
2018-07-13 12:11:57 -0400

    actualizar trabajos
    agregar y arreglar cambios y contribucion christian

--- src/Modelo/Trabajo.java
+++ src/Modelo/Trabajo.java
@@ -5,25 +5,22 @@
  */
 package Modelo;
 
-/**
- *
- * @author krlos
- */
+
 public class Trabajo{
     private String nivel;
     private String titulo;
-    private int ci;
+    private String ci;
     private String Apellido1;
     private String Nombre1;
-    private int ci_2;
+    private String ci_2;
     private String Apellido_2;
     private String Nombre_2;
     private String sem1erainsc;
     private String fechaDefensa;
-    private int ci_t;
+    private String ci_t;
     private String Apellido_t1;
     private String Nombre_t1;
-    private int ci_t2;
+    private String ci_t2;
     private String Apellido_t2;
     private String Nombre_t2;
     
@@ -35,10 +32,184 @@ public class Trabajo{
     public String getFechaDefensa() {
         return fechaDefensa;
     }
-    public int getCi_t() {
+    public String getCi_t() {
         return ci_t;
     }
-    public int getCi_t2(){
+    public String getCi_t2(){
         return ci_t2;
     }
-}
+    
+    public int llenarCampos(String linea)
+    {
+       int ls_str=4, li_str=4;
+       //asignar a campos OBLIGATORIOS 1
+               nivel = linea.substring(0, 3);
+                if(!nivel.equalsIgnoreCase("TEG") && !nivel.equalsIgnoreCase("TGM") && !nivel.equalsIgnoreCase("TDR")){
+                    return -1;
+                }
+                                
+                while(linea.charAt(ls_str) != '#') {ls_str++;}
+                titulo = linea.substring(li_str, ls_str);
+                
+                li_str = ls_str;
+                ++ls_str; 
+                ++li_str;
+             
+                while(linea.charAt(ls_str) != '#') {ls_str++;}
+                ci = linea.substring(li_str, ls_str);
+                     
+                li_str = ls_str;
+                ++ls_str; 
+                ++li_str;
+                
+                //campos opcionales:
+                
+                //campos opcionales:
+                //[apellido y nombre 1]
+                while(linea.charAt(ls_str) != '#') {ls_str++;}
+                Apellido1 = linea.substring(li_str, ls_str);
+                
+                 li_str = ls_str;
+                ++ls_str; 
+                ++li_str;
+                
+               
+                
+                while(linea.charAt(ls_str) != '#') {ls_str++;}
+                Nombre1 = linea.substring(li_str, ls_str);
+                
+                li_str = ls_str;
+                    ++ls_str; 
+                    ++li_str;
+                
+                    
+                if(nivel.equalsIgnoreCase("TEG")) {
+                    //[ci_2, nombre2 y apellido2]
+                    
+                    if(linea.charAt(li_str) != linea.charAt(li_str+1)) {
+                      while(linea.charAt(ls_str) != '#') {ls_str++;}
+                         ci_2 = linea.substring(li_str, ls_str);
+                    } else {
+                          ci_2 = " ";
+                    }
+                    
+                    
+                    
+                    li_str = ls_str;
+                    ++ls_str; 
+                    ++li_str;
+                
+                    if(linea.charAt(li_str) != linea.charAt(li_str+1)) {
+                         while(linea.charAt(ls_str) != '#') {ls_str++;}
+                        Apellido_2 = linea.substring(li_str, ls_str);
+                   } else {
+                         Apellido_2 = " ";
+                     }
+                     li_str = ls_str;
+                    ++ls_str; 
+                    ++li_str;
+                    
+                    
+                    if(linea.charAt(li_str) != linea.charAt(li_str+1)) {
+                         while(linea.charAt(ls_str) != '#') {ls_str++;}
+                        Nombre_2 = linea.substring(li_str, ls_str);
+                    } else {
+                         Nombre_2 =" ";
+                    }
+                    li_str = ls_str;
+                ++ls_str; 
+                ++li_str;
+                } else {
+                    Apellido_2 = Nombre_2 = " ";
+                    ci_2 = " ";
+                }
+                //sem1erains
+                while(linea.charAt(ls_str) != '#') {ls_str++;}
+                sem1erainsc = linea.substring(li_str, ls_str);
+                li_str = ls_str;
+                ++ls_str; 
+                ++li_str;
+                //fechaDefensa 
+                while(linea.charAt(ls_str) != '#') {ls_str++;}
+                fechaDefensa = linea.substring(li_str, ls_str);
+                li_str = ls_str;
+                ++ls_str; 
+                ++li_str;
+                
+                //ci t
+                while(linea.charAt(ls_str) != '#') {ls_str++;}
+                ci_t = linea.substring(li_str, ls_str);
+                 
+                //NUEVO:
+                li_str = ls_str;
+                    --ls_str; 
+                    --li_str;
+                
+                    if(linea.charAt(li_str) != linea.charAt(li_str+1)) {
+                         while(linea.charAt(ls_str) != '#') {ls_str++;}
+                       Apellido_t1 = linea.substring(li_str, ls_str);
+                   } else {
+                         Apellido_t1 = " ";
+                     }
+                    System.out.println("Listo at1"+Apellido_t1);
+                     li_str = ls_str;
+                    ++ls_str; 
+                    ++li_str;
+                    
+                    
+                    if(linea.charAt(li_str) != linea.charAt(li_str+1)) {
+                         while(linea.charAt(ls_str) != '#') {ls_str++;}
+                        Nombre_t1 = linea.substring(li_str, ls_str);
+                    } else {
+                        Nombre_t1 =" ";
+                    }
+                    System.out.println("Listo nt1"+Nombre_t1);
+                    li_str = ls_str;
+                ++ls_str; 
+                ++li_str;
+                    
+                if(linea.charAt(li_str) != linea.charAt(li_str+1)) {
+                         while(linea.charAt(ls_str) != '#') {ls_str++;}
+                        ci_t2 = linea.substring(li_str, ls_str);
+                    } else {
+                         ci_t2 =" ";
+                    }
+                System.out.println("Listo ci_t2"+ci_t2);
+                    li_str = ls_str;
+                ++ls_str; 
+                ++li_str;
+                
+                if(linea.charAt(li_str) != linea.charAt(li_str+1)) {
+                         while(linea.charAt(ls_str) != '#') {ls_str++;}
+                        Apellido_t2 = linea.substring(li_str, ls_str);
+                    } else {
+                         Apellido_t2 =" ";
+                    }
+                System.out.println("Listo at2"+Apellido_t2);
+                    li_str = ls_str;
+                ++ls_str; 
+                ++li_str;
+                
+                if(linea.charAt(li_str) != linea.charAt(li_str+1)) {
+                         while(linea.charAt(ls_str) != ';') {ls_str++;}
+                        Nombre_t2 = linea.substring(li_str, ls_str);
+                    } else {
+                         Nombre_t2 =" ";
+                    }
+                    System.out.println("Listo nt1"+Nombre_t2);
+                
+                
+                System.out.println("ci:"+ci);    
+                 System.out.println("apellido1: "+Apellido1);
+                System.out.println("nombre1: "+Nombre1);
+                System.out.println("ci_2:"+ci_2);             
+                 System.out.println("apellido2: "+Apellido_2);       
+                System.out.println("nombre2: "+Nombre_2);
+                System.out.println("sem1erainsc:"+sem1erainsc);
+                System.out.println("fechaDefensa:"+fechaDefensa);
+                System.out.println("ci_t: "+ci_t);
+                //reiniciar apuntadores de subcadenas
+             
+             return 1;
+   }  
+} 
