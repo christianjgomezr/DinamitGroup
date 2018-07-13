63adb27bb1a33c101076694b3d90e5be06795728
Sagnior <38483140+Sagnior@users.noreply.github.com>
2018-07-12 21:36:00 -0400

    Create ConjuntoProfesores.java

---
+++ src/Modelo/ConjuntoProfesores.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+import java.util.ArrayList;
+/**
+ *
+ * @author Ugueto
+ */
+public class ConjuntoProfesores {
+    private ArrayList<Profesor> listado;
+    
+    public void agregar(Profesor P){
+        listado.add(P);
+    }
+}
