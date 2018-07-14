4c918429c436c05e2288004c21eb7413bdfc19ab
christianjgomezr <38545643+christianjgomezr@users.noreply.github.com>
2018-07-13 23:26:12 -0400

    Update ReportePostgrados.java
    listo saul!!

--- src/Modelo/ReportePostgrados.java
+++ src/Modelo/ReportePostgrados.java
@@ -8,48 +8,24 @@ package Modelo;
  *
  * @author Ugueto
  */
-public class ReportePostgrados extends Reporte{
-    
+public class ReportePostgrados extends Reporte{    
     
     public void ordenar(){
         Profesor aux = new Profesor();
         for(int i=ListaProf.size()-1; i>0 ;i--){      
             for(int j=0; j<i ;j++){
-                if(ListaProf.get(j).Apellido.compareTo(ListaProf.get(j+1).Apellido)==0){ 
+                if(ListaProf.get(j).getApellido().compareTo(ListaProf.get(j+1).getApellido())==0){ 
                     
-                    if(ListaProf.get(j).Nombre.compareTo(ListaProf.get(j+1).Nombre)<0){ //pasa al nombre
-                        aux.Apellido=ListaProf.get(j).Apellido;
-                        aux.Nombre=ListaProf.get(j).Nombre;
-                        aux.centro=ListaProf.get(j).centro;
-                        aux.ci=ListaProf.get(j).ci;
-
-                        ListaProf.get(j).Apellido=ListaProf.get(j+1).Apellido;
-                        ListaProf.get(j).Nombre=ListaProf.get(j+1).Nombre;
-                        ListaProf.get(j).centro=ListaProf.get(j+1).centro;
-                        ListaProf.get(j).ci=ListaProf.get(j+1).ci;
-
-                        ListaProf.get(j+1).Apellido=aux.Apellido;
-                        ListaProf.get(j+1).Nombre=aux.Nombre;
-                        ListaProf.get(j+1).centro=aux.centro;
-                        ListaProf.get(j+1).ci=aux.ci;
+                    if(ListaProf.get(j).getNombre().compareTo(ListaProf.get(j+1).getNombre())<0){ //pasa al nombre
+                       aux = ListaProf.get(j);
+                       ListaProf.set(j, ListaProf.get(j+1));
+                       ListaProf.set(j+1, aux);
                     }
                 }
-                if(ListaProf.get(j).Apellido.compareTo(ListaProf.get(j+1).Apellido) < 0){
-                    
-                    aux.Apellido=ListaProf.get(j).Apellido;
-                    aux.Nombre=ListaProf.get(j).Nombre;
-                    aux.centro=ListaProf.get(j).centro;
-                    aux.ci=ListaProf.get(j).ci;
-                    
-                    ListaProf.get(j).Apellido=ListaProf.get(j+1).Apellido;
-                    ListaProf.get(j).Nombre=ListaProf.get(j+1).Nombre;
-                    ListaProf.get(j).centro=ListaProf.get(j+1).centro;
-                    ListaProf.get(j).ci=ListaProf.get(j+1).ci;
-                    
-                    ListaProf.get(j+1).Apellido=aux.Apellido;
-                    ListaProf.get(j+1).Nombre=aux.Nombre;
-                    ListaProf.get(j+1).centro=aux.centro;
-                    ListaProf.get(j+1).ci=aux.ci;
+                if(ListaProf.get(j).getApellido().compareTo(ListaProf.get(j+1).getApellido()) < 0){
+                       aux = ListaProf.get(j);
+                       ListaProf.set(j, ListaProf.get(j+1));
+                       ListaProf.set(j+1, aux);
                 }
             }
         } 
